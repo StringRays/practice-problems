fe9a4cedcb45f25266249ccb4cacaa40f68cbea4
Rhea <101016117+StringRays@users.noreply.github.com>
2022-07-29 10:33:39 -0500

    Cleaned up

--- javascript/other.js
+++ javascript/other.js
@@ -16,65 +16,65 @@ function totalBasketballScore(freeThrows, midRange, threePointers) {
     totalBasketballScore(2, 3, 4)
     
     
-    //number two
-    function itemPrice(item) {
-        //checks item type
-        if (typeof item !== 'string') {
-           console.log("All entries must be of type string");
-        }
-        switch(item){
-            case 'computer': 
-                return '$500';
-                break;
-            case 'mouse': 
-                return  '$10';            
-                break;
-            case 'tablet': 
-                return '$250';
-                break;
-            case 'case': 
-                return  '$25';
-                break;
-            case 'router': 
-                return  '$100';
-                break;
-            default:
-                console.log('Item not in store');
+ //number two
+function itemPrice(item) {
+    //checks item type
+    if (typeof item !== 'string') {
+    console.log("All entries must be of type string");
+     }
+    switch(item){
+        case 'computer': 
+            return '$500';
+            break;
+        case 'mouse': 
+            return  '$10';            
+            break;
+        case 'tablet': 
+            return '$250';
+            break;
+        case 'case': 
+            return  '$25';
+            break;
+        case 'router': 
+            return  '$100';
+            break;
+        default:
+            console.log('Item not in store');
         }
     }
     itemPrice('mouse')
     
     
-    
-    function randomRestaurant(numOfRestaurants) {
-        let restaurants = ['Applebees', "Panera Bread", "Popeyes", "Chopt", "Chick-fil-A", "Cheesecake Factory", "Five Guys"];
-        let randomRestaurantList = [];
-        //pushes a random restaurant from the list onto the random list the number of times of the input number
-        for (let count = 0; count < numOfRestaurants; count++) {
-                randomRestaurantList.push(restaurants[Math.round(Math.random()*restaurants.length)]);
-            }
-        return randomRestaurantList;
-    }
+//problem three
+function randomRestaurant(numOfRestaurants) {
+    let restaurants = ['Applebees', "Panera Bread", "Popeyes", "Chopt", "Chick-fil-A", "Cheesecake Factory", "Five Guys"];
+    let randomRestaurantList = [];
+    //pushes a random restaurant from the list onto the random list the number of times of the input number
+    for (let count = 0; count < numOfRestaurants; count++) {
+            randomRestaurantList.push(restaurants[Math.round(Math.random()*restaurants.length)]);
+        }
+    return randomRestaurantList;
+ }
     
     randomRestaurant(55);
     
     
     
-    
-    function isLeapYear(year) {
-        //if year is divisible by 100, checks if divisible by 400
-        if (year%100 ==0 ){
-            if (year%400 ==0) {
-                return true;
-            }
-            return false;
-        } 
-        //Otherwise, checks if divisible by 4
-        else if (year%4 ==0) {
+//problem four
+function isLeapYear(year) {
+    //if year is divisible by 100, checks if divisible by 400
+    if (year%100 ==0 ){
+        if (year%400 ==0) {
             return true;
-        } else {
-            return false;
         }
+        return false;
+    } 
+    //Otherwise, checks if divisible by 4
+    else if (year%4 ==0) {
+        return true;
+    } else {
+        return false;
+     }
     }
     
     isLeapYear(1976);
